5f0fb4f3f0d197dba5eeeb7d7c110350812045fb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-07-10 22:52:13 +0900

    Update myv.sh

--- myv.sh
+++ myv.sh
@@ -27,6 +27,10 @@
 # 2022.07.08
 # Update : Add FS2500 jun mode
 # 2022.07.10
+
+##### INCLUDES #########################################################################################################
+. include/common.sh # my.sh / myv.sh common use 
+########################################################################################################################
                                                                                         
 mshellgz="myv.sh.gz"
 mshtarfile="https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/main/myv.sh.gz"
@@ -50,88 +54,6 @@ READ_YN () { # $1:question $2:default
     *) echo -e "\n" ;;                                                                                                        
     esac                                                                                                                      
 }                                                                                         
-# ==============================================================================          
-# Color Function                                                                          
-# ==============================================================================          
-cecho () {                                                                                
-    if [ -n "$3" ]                                                                                                            
-    then                                                                                  
-        case "$3" in                                                                                 
-            black  | bk) bgcolor="40";;                                                              
-            red    |  r) bgcolor="41";;                                                              
-            green  |  g) bgcolor="42";;                                                                 
-            yellow |  y) bgcolor="43";;                                             
-            blue   |  b) bgcolor="44";;                                             
-            purple |  p) bgcolor="45";;                                                   
-            cyan   |  c) bgcolor="46";;                                             
-            gray   | gr) bgcolor="47";;                                             
-        esac                                                                        
-    else                                                                            
-        bgcolor="0"                                                                 
-    fi                                                                              
-    code="\033["                                                                    
-    case "$1" in                                                                    
-        black  | bk) color="${code}${bgcolor};30m";;                                
-        red    |  r) color="${code}${bgcolor};31m";;                                
-        green  |  g) color="${code}${bgcolor};32m";;                                
-        yellow |  y) color="${code}${bgcolor};33m";;                                
-        blue   |  b) color="${code}${bgcolor};34m";;                                
-        purple |  p) color="${code}${bgcolor};35m";;                                
-        cyan   |  c) color="${code}${bgcolor};36m";;                                
-        gray   | gr) color="${code}${bgcolor};37m";;                                
-    esac                                                                            
-                                                                                                                                                                    
-    text="$color$2${code}0m"                                                                                                                                        
-    echo -e "$text"                                                                                                                                                 
-}   
-
-function checkinternet() {
-
-    echo -n "Checking Internet Access -> "
-    nslookup github.com 2>&1 >/dev/null
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    else
-        cecho g "Error: No internet found, or github is not accessible"
-        exit 99
-    fi
-
-}
-
-function getlatestmshell() {
-
-    echo -n "Checking if a newer mshell version exists on the repo -> "
-
-    if [ ! -f $mshellgz ]; then
-        curl -s --location "$mshtarfile" --output $mshellgz
-    fi
-
-    curl -s --location "$mshtarfile" --output latest.mshell.gz
-
-    CURRENTSHA="$(sha256sum $mshellgz | awk '{print $1}')"
-    REPOSHA="$(sha256sum latest.mshell.gz | awk '{print $1}')"
-
-    if [ "${CURRENTSHA}" != "${REPOSHA}" ]; then
-        echo -n "There is a newer version of m shell script on the repo should we use that ? [yY/nN]"
-        read confirmation
-        if [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]; then
-            echo "OK, updating, please re-run after updating"
-            cp -f /home/tc/latest.mshell.gz /home/tc/$mshellgz
-            rm -f /home/tc/latest.mshell.gz
-            tar -zxvf $mshellgz
-            echo "Updating m shell with latest updates"
-            exit
-        else
-            rm -f /home/tc/latest.mshell.gz
-            return
-        fi
-    else
-        echo "Version is current"
-        rm -f /home/tc/latest.mshell.gz
-    fi
-
-}
-
 
 # Function EXDRIVER_FN
 # Made by FOXBI
@@ -228,58 +150,6 @@ function EXDRIVER_FN() {
         READ_YN "Do you want add driver? Y/N :  "
         ICHK=$Y_N
     done
-}
-
-function showhelp() {
-    cat <<EOF
-$(basename ${0})
-
-----------------------------------------------------------------------------------------
-Usage: ${0} <Synology Model Name> <Options>
-
-Options: postupdate, noconfig, noclean, manual
-
-- postupdate : Option to patch the restore loop after applying DSM 7.1.0-42661 Update 2, no additional build required.
-
-- noconfig: SKIP automatic detection change processing such as SN/Mac/Vid/Pid/SataPortMap of user_config.json file.
-
-- noclean: SKIP the 💊   RedPill LKM/LOAD directory without clearing it with the Clean now command. 
-           However, delete the Cache directory and loader.img.
-
-- manual: Options for manual extension processing and manual dtc processing in build action (skipping extension auto detection)
-
-Please type Synology Model Name after ./$(basename ${0})
-
-- for jot mode
-
-./$(basename ${0}) DS918+
-./$(basename ${0}) DS3617xs
-./$(basename ${0}) DS3615xs
-./$(basename ${0}) DS3622xs+
-./$(basename ${0}) DVA3221
-./$(basename ${0}) DS920+
-./$(basename ${0}) DS1621+
-./$(basename ${0}) DS2422+ 
-./$(basename ${0}) DVA1622
-./$(basename ${0}) DS1520+ (Not Suppoted)
-./$(basename ${0}) FS2500 (Not Suppoted)
-
-- for jun mode
-
-./$(basename ${0}) DS918+J                                                                                                      
-./$(basename ${0}) DS3617xsJ                                                                                                    
-./$(basename ${0}) DS3615xsJ                                                                                                    
-./$(basename ${0}) DS3622xs+J                                                                                                   
-./$(basename ${0}) DVA3221J                                                                                                     
-./$(basename ${0}) DS920+J                                                                                                      
-./$(basename ${0}) DS1621+J
-./$(basename ${0}) DS2422+J  
-./$(basename ${0}) DVA1622J (Not Suppoted)
-./$(basename ${0}) DS1520+J
-./$(basename ${0}) FS2500J
-
-EOF
-
 }
 
 checkinternet
