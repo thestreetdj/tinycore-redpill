8f671c22e59d60d31cdb0c07b8f473251ac04821
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-07-10 23:57:40 +0900

    Update myv.sh

--- myv.sh
+++ myv.sh
@@ -3,33 +3,9 @@
 # myv.sh (Batch Shell Script for rploader.sh)                 
 # Made by Peter Suh
 # 2022.04.18                      
-# Update : add 42661 U1 NanoPacked
-# 2022.04.28
-# Update : add noconfig, noclean, manual options
-# 2022.04.30
-# Update : add noconfig, noclean, manual combinatione options
-# 2022.05.06   
-# Update : add pat file sha256 check                         
-# 2022.05.07      
-# Update : Added dtc compilation function for user custom dts file
-# 2022.05.15
-# Update : call connection with my.sh
-# 2022.05.25
-# Update : Add jumkey's Jun mode
-# 2022.06.11
-# Update : Add DS2422+ jun mode Support
-# 2022.06.28
-# Update : Add DS2422+ jot mode Support
-# 2022.07.02
-# Update : Add DVA1622 jun mode (Testing)
-# 2022.07.07
-# Update : Add DS1520+ jun mode
-# 2022.07.08
-# Update : Add FS2500 jun mode
-# 2022.07.10
 
 ##### INCLUDES #########################################################################################################
-. include/common.sh # my.sh / myv.sh common use 
+source include/common.sh # my.sh / myv.sh common use 
 ########################################################################################################################
                                                                                         
 mshellgz="myv.sh.gz"
