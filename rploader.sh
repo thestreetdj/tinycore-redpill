bab86a0c27442dcfc3c6bbcb1b1d6994db06230f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-08-06 12:50:15 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -454,7 +454,7 @@ function addrequiredexts() {
         cd /home/tc/redpill-load/ && ./ext-manager.sh _update_platform_exts ${SYNOMODEL} ${extension}
     done
 
- Use user define dts file instaed of dtbpatch ext now
+ #Use user define dts file instaed of dtbpatch ext now
     if [ ${TARGET_PLATFORM} = "geminilake" ] || [ ${TARGET_PLATFORM} = "v1000" ] || [ ${TARGET_PLATFORM} = "dva1622" ] || [ ${TARGET_PLATFORM} = "ds2422p" ] || [ ${TARGET_PLATFORM} = "ds1520p" ] ; then
         echo "For user define dts file instaed of dtbpatch ext"
         patchdtc
