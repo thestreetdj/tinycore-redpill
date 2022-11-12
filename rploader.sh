b530caf631ccf6bbbe13e0e23ad3e988c81322b3
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-12 14:55:17 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -811,8 +811,12 @@ bringfriend() {
 
             cp $userconfigfile /mnt/${loaderdisk}3/
 
+#m shell only start
+            sudo sed -i '61,80d' /mnt/${loaderdisk}1/boot/grub/grub.cfg
+#m shell only end
+
             echo "Setting default boot entry to TCRP Friend"
-            sudo sed -i "/set default=\"*\"/cset default=\"3\"" /mnt/${loaderdisk}1/boot/grub/grub.cfg
+            sudo sed -i "/set default=\"*\"/cset default=\"1\"" /mnt/${loaderdisk}1/boot/grub/grub.cfg
 
             if [ ! -f /mnt/${loaderdisk}3/bzImage-friend ] || [ ! -f /mnt/${loaderdisk}3/initrd-friend ] || [ ! -f /mnt/${loaderdisk}3/zImage-dsm ] || [ ! -f /mnt/${loaderdisk}3/initrd-dsm ] || [ ! -f /mnt/${loaderdisk}3/user_config.json ] || [ ! $(grep -i "Tiny Core Friend" /mnt/${loaderdisk}1/boot/grub/grub.cfg | wc -l) -eq 1 ]; then
                 echo "ERROR !!! Something went wrong, please re-run the process"
@@ -2413,9 +2417,9 @@ EOF
 }
 
 function tcrpfriendentry() {
-
+    
     cat <<EOF
-menuentry 'Tiny Core Friend' {
+menuentry 'Tiny Core Friend $MODEL ${TARGET_VERSION}-${TARGET_REVISION} Update ${smallfixnumber}' {
         savedefault
         set root=(hd0,msdos3)
         echo Loading Linux...
@@ -2820,6 +2824,10 @@ function buildloader() {
 
     cp $userconfigfile /mnt/${loaderdisk}3/
 
+#m shell only start
+    sudo sed -i '61,80d' /home/tc/redpill-load/localdiskp1/boot/grub/grub.cfg
+#m shell only end
+
     if [ "$WITHFRIEND" = "YES" ]; then
 
         cp localdiskp1/zImage /mnt/${loaderdisk}3/zImage-dsm
@@ -2850,15 +2858,14 @@ function buildloader() {
         fi
 
         echo "Setting default boot entry to TCRP Friend"
-        cd /home/tc/redpill-load/ && sudo sed -i "/set default=\"*\"/cset default=\"3\"" localdiskp1/boot/grub/grub.cfg
+        cd /home/tc/redpill-load/ && sudo sed -i "/set default=\"*\"/cset default=\"1\"" localdiskp1/boot/grub/grub.cfg
 
     else
-
-        if [ "$MACHINE" = "VIRTUAL" ]; then
-            echo "Setting default boot entry to SATA"
-            cd /home/tc/redpill-load/ && sudo sed -i "/set default=\"*\"/cset default=\"1\"" localdiskp1/boot/grub/grub.cfg
-        fi
-
+        echo
+#        if [ "$MACHINE" = "VIRTUAL" ]; then
+#            echo "Setting default boot entry to SATA"
+#            cd /home/tc/redpill-load/ && sudo sed -i "/set default=\"*\"/cset default=\"1\"" localdiskp1/boot/grub/grub.cfg
+#        fi
     fi
 
 #m shell only start
@@ -2867,8 +2874,6 @@ function buildloader() {
     sudo mv localdiskp2/custom.gz /mnt/${loaderdisk}3 
     echo "Copy zImage to partition 3"    
     cp localdiskp1/zImage         /mnt/${loaderdisk}3     
-    echo "Copy bzImage to partition 3"    
-    cp localdiskp2/bzImage        /mnt/${loaderdisk}3     
 #m shell only end
 
     cd /home/tc/redpill-load/
@@ -2876,13 +2881,6 @@ function buildloader() {
     ####
 
     checkmachine
-
-#m shell only start
-    if [ "$MACHINE" = "VIRTUAL" ]; then
-        echo "Setting default boot entry to SATA"
-        sudo sed -i "/set default=\"0\"/cset default=\"1\"" localdiskp1/boot/grub/grub.cfg
-    fi
-#m shell only end
 
     sudo umount part1
     sudo umount part2
