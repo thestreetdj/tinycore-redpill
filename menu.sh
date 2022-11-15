ace67656f7980d8888cac422af219d5cd1125e3d
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-15 22:14:52 +0900

    Add delay and fix identifyusb function

--- menu.sh
+++ menu.sh
@@ -4,6 +4,25 @@
 #source /home/tc/menufunc.h
 #####################################################################################################
 
+function checkmachine() {
+
+    if grep -q ^flags.*\ hypervisor\  /proc/cpuinfo; then
+        MACHINE="VIRTUAL"
+        HYPERVISOR=$(dmesg | grep -i "Hypervisor detected" | awk '{print $5}')
+        echo "Machine is $MACHINE Hypervisor=$HYPERVISOR"
+    fi
+
+}
+
+checkmachine
+
+# interval for loading somthing...
+if [ "$MACHINE" = "VIRTUAL" ]; then
+    sleep 1
+else
+    sleep 3
+fi
+
 [[ "$(which dialog)_" == "_" ]] && tce-load -wi dialog
 
 # Get actual IP
@@ -81,16 +100,6 @@ function backtitle() {
   echo ${BACKTITLE}
 }
 
-function checkmachine() {
-
-    if grep -q ^flags.*\ hypervisor\  /proc/cpuinfo; then
-        MACHINE="VIRTUAL"
-        HYPERVISOR=$(dmesg | grep -i "Hypervisor detected" | awk '{print $5}')
-        echo "Machine is $MACHINE Hypervisor=$HYPERVISOR"
-    fi
-
-}
-
 function usbidentify() {
 
     checkmachine
@@ -110,6 +119,44 @@ function usbidentify() {
         sed -i "/\"vid\": \"/c\    \"vid\": \"$vendorid\"," user_config.json
 	    
         exit 0
+    fi
+    
+    loaderdisk=$(mount | grep -i optional | grep cde | awk -F / '{print $3}' | uniq | cut -c 1-3)
+
+    lsusb -v 2>&1 | grep -B 33 -A 1 SCSI >/tmp/lsusb.out
+
+    usblist=$(grep -B 33 -A 1 SCSI /tmp/lsusb.out)
+    vendorid=$(grep -B 33 -A 1 SCSI /tmp/lsusb.out | grep -i idVendor | awk '{print $2}')
+    productid=$(grep -B 33 -A 1 SCSI /tmp/lsusb.out | grep -i idProduct | awk '{print $2}')
+
+    if [ $(echo $vendorid | wc -w) -gt 1 ]; then
+        echo "Found more than one USB disk devices, please select which one is your loader on"
+        usbvendor=$(for item in $vendorid; do grep $item /tmp/lsusb.out | awk '{print $3}'; done)
+        select usbdev in $usbvendor; do
+            vendorid=$(grep -B 10 -A 10 $usbdev /tmp/lsusb.out | grep idVendor | grep $usbdev | awk '{print $2}')
+            productid=$(grep -B 10 -A 10 $usbdev /tmp/lsusb.out | grep -A 1 idVendor | grep idProduct | awk '{print $2}')
+            echo "Selected Device : $usbdev , with VendorID: $vendorid and ProductID: $productid"
+            break
+        done
+    else
+        usbdevice="$(grep iManufacturer /tmp/lsusb.out | awk '{print $3}') $(grep iProduct /tmp/lsusb.out | awk '{print $3}') SerialNumber: $(grep iSerial /tmp/lsusb.out | awk '{print $3}')"
+    fi
+
+    if [ -n "$usbdevice" ] && [ -n "$vendorid" ] && [ -n "$productid" ]; then
+        echo "Found $usbdevice"
+        echo "Vendor ID : $vendorid Product ID : $productid"
+
+        echo "Should i update the user_config.json with these values ? [Yy/Nn]"
+        read answer
+        if [ -n "$answer" ] && [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+            json="$(jq --arg var "$productid" '.extra_cmdline.pid = $var' user_config.json)" && echo -E "${json}" | jq . >user_config.json
+            json="$(jq --arg var "$vendorid" '.extra_cmdline.vid = $var' user_config.json)" && echo -E "${json}" | jq . >user_config.json
+        else
+            echo "OK remember to update manually by editing user_config.json file"
+        fi
+    else
+        echo "Sorry, no usb disk could be identified"
+        rm /tmp/lsusb.out
     fi
 }
 
