3d59c00e8678152341b2bfefe3fc7bc8bf6c62e4
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-16 11:41:55 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -2811,7 +2811,7 @@ function buildloader() {
     cp $userconfigfile /mnt/${loaderdisk}3/
 
 #m shell only start
-    sudo sed -i '61,80d' /home/tc/redpill-load/localdiskp1/boot/grub/grub.cfg
+#    sudo sed -i '61,80d' /home/tc/redpill-load/localdiskp1/boot/grub/grub.cfg
 #m shell only end
 
     if [ "$WITHFRIEND" = "YES" ]; then
