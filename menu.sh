0ac2969e0092f181f4fb1308791576ac951b6668
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-18 14:07:35 +0900

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -41,11 +41,10 @@ MODEL="$(jq -r -e '.general.model' $USER_CONFIG_FILE)"
 BUILD="42962"
 SN="$(jq -r -e '.extra_cmdline.sn' $USER_CONFIG_FILE)"
 MACADDR1="$(jq -r -e '.extra_cmdline.mac1' $USER_CONFIG_FILE)"
+NETNUM="1"
 if [ $(ifconfig | grep eth1 | wc -l) -gt 0 ]; then
   MACADDR2="$(jq -r -e '.extra_cmdline.mac2' $USER_CONFIG_FILE)"
   NETNUM="2"
-else
-  NETNUM="1"
 fi
 
 LAYOUT="$(jq -r -e '.general.layout' $USER_CONFIG_FILE)"
