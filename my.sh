4d62a613c1493dc925201d29834125dbaa954dc9
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-19 17:21:28 +0900

    Update my.sh

--- my.sh
+++ my.sh
@@ -74,6 +74,7 @@ if [ $# -lt 1 ]; then
 else
     if [ "$1" == "update" ] ; then 
         getlatestmshell "noask"
+        echo "y"|./rploader.sh backup        
         exit 0
     else
         getlatestmshell "ask"
