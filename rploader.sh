44ebbf48ec174e362342636bd695888017a8bd83
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-01-03 01:18:43 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -1775,6 +1775,10 @@ function backup() {
     loaderdisk=$(mount | grep -i optional | grep cde | awk -F / '{print $3}' | uniq | cut -c 1-3)
     homesize=$(du -sh /home/tc | awk '{print $1}')
 
+if [ $loaderdisk == "mmc" ]; then
+    loaderdisk="mmcblk0p"
+fi  
+
     echo "Please make sure you are using the latest 1GB img before using backup option"
     echo "Current /home/tc size is $homesize , try to keep it less than 1GB as it might not fit into your image"
 
