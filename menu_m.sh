0221249556f3314c5c2dfef895e59a75dbb5b542
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-02-20 14:39:22 +0900

    Add DS1520+ 7.0.1-42218

--- menu_m.sh
+++ menu_m.sh
@@ -555,7 +555,7 @@ while true; do
     fi
     echo "j \"Build the [TCRP JOT Mod] loader\""            >> "${TMP_PATH}/menu"   
     echo "p \"Post Update for [TCRP JOT Mod]\""             >> "${TMP_PATH}/menu"   
-    if [ "${MODEL}" == "DS918+" ]||[ "${MODEL}" == "DS1019+" ]||[ "${MODEL}" == "DS920+" ]; then        
+    if [ "${MODEL}" == "DS918+" ]||[ "${MODEL}" == "DS1019+" ]||[ "${MODEL}" == "DS920+" ]||[ "${MODEL}" == "DS1520+" ]; then        
     	echo "o \"Build the [TCRP FRIEND 7.0.1-42218] loader\""  >> "${TMP_PATH}/menu"    
     fi	
   fi
