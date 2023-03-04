7e6d6aaa1d70f0ed09fa6dad146e53e97560ebe7
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-04 12:31:44 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -705,7 +705,7 @@ while true; do
   echo "r \"Reboot\""                                 >> "${TMP_PATH}/menu"
   echo "e \"Exit\""                                   >> "${TMP_PATH}/menu"
   dialog --clear --default-item ${NEXT} --backtitle "`backtitle`" --colors \
-    --menu "\e[32mDevice-Tree[DT] Base Models & HBAs do not require SataPortMap,DiskIdxMap\nDT models do not support HBAs\e[0m\n${result}" 0 0 0 --file "${TMP_PATH}/menu" \
+    --menu "Device-Tree[DT] Base Models & HBAs do not require SataPortMap,DiskIdxMap\nDT models do not support HBAs\n${result}" 0 0 0 --file "${TMP_PATH}/menu" \
     2>${TMP_PATH}/resp
   [ $? -ne 0 ] && break
   case `<"${TMP_PATH}/resp"` in
