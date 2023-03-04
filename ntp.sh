de953019e3364ef7a23fb8a6a086b59e81a5e551
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-04 22:49:57 +0900

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -7,6 +7,11 @@ if [ "$(which ntpclient)_" == "_" ]; then
     tce-load -iw ntpclient 2>&1 >/dev/null
 fi    
 export TZ="${timezone}"
-echo "Set DateTime Sync with $ntpserver"
+echo "Synchronizing dateTime with ntp server $ntpserver ......"
 sudo ntpclient -s -h ${ntpserver} 2>&1 >/dev/null
+echo
 echo "DateTime synchronization complete!!!"
+echo
+echo "press any key to continue..."                                                                                                   
+read answer
+exit 0
