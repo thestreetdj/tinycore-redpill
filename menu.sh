42e89e8d9d189fc48990e24af755bd3834149400
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-14 07:49:08 +0900

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function gitdownload() {
+
+    git config --global http.sslVerify false    
+    if [ -d /home/tc/redpill-load ]; then
+        echo "Loader sources already downloaded, pulling latest"
+        cd /home/tc/redpill-load
+        git pull
+        cd /home/tc
+    else
+        git clone -b master "https://github.com/PeterSuh-Q3/redpill-load.git"        
+    fi
+
+}
+
 while true; do
   if [ $(ifconfig | grep -i "inet " | grep -v "127.0.0.1" | wc -l) -gt 0 ]; then
     /home/tc/my.sh update
@@ -8,6 +22,8 @@ while true; do
   sleep 1
   echo "Waiting for internet activation in menu.sh !!!"
 done
+
+gitdownload
 
 /home/tc/menu_m.sh
 exit 0
