4857998e44b471daddbe538e14346ff2994e3404
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-25 18:08:15 +0900

    Update my.sh

--- my.sh
+++ my.sh
@@ -132,14 +132,6 @@ function chkavail() {
     echo "Avail space ${avail_num}M on /mnt/${tcrppart}"
 }
 
-sed -i "s/screen_color = (CYAN,GREEN,ON)/screen_color = (CYAN,BLUE,ON)/g" .dialogrc
-echo "insert aterm menu.sh in /home/tc/.xsession"
-sed -i "/aterm/d" .xsession
-echo "aterm -geometry 78x32+10+0 -fg yellow -title \"TCRP Monitor\" -e /home/tc/rploader.sh monitor &" >> .xsession
-echo "aterm -geometry 78x32+525+0 -title \"M Shell for TCRP Menu\" -e /home/tc/menu.sh &" >> .xsession
-echo "aterm -geometry 78x25+10+430 -fg orange -title \"TCRP NTP Sync\" -e /home/tc/ntp.sh &" >> .xsession
-echo "aterm -geometry 78x25+525+430 -fg green -title \"TCRP Extra Terminal\" &" >> .xsession
-
 checkinternet
 gitdownload
 
