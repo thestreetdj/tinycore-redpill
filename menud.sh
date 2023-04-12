a7c1bb972ce22e048b72256d2d1769a310f869c5
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-04-12 11:04:11 +0900

    Create menud.sh

---
+++ menud.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function gitdownload() {
+
+    git config --global http.sslVerify false    
+    if [ -d /home/tc/redpill-load ]; then
+        echo "Loader sources already downloaded, pulling latest"
+        cd /home/tc/redpill-load
+        git pull
+        if [ $? -ne 0 ]; then
+           cd /home/tc    
+           /home/tc/rploader.sh clean 
+           git clone -b master "https://giteas.duckdns.org/PeterSuh-Q3/redpill-load.git"
+        fi   
+        cd /home/tc
+    else
+        git clone -b master "https://giteas.duckdns.org/PeterSuh-Q3/redpill-load.git"        
+    fi
+
+}
+
+while true; do
+  if [ $(ifconfig | grep -i "inet " | grep -v "127.0.0.1" | wc -l) -gt 0 ]; then
+    /home/tc/my.sh update
+    break
+  fi
+  sleep 1
+  echo "Waiting for internet activation in menu.sh !!!"
+done
+
+gitdownload
+
+/home/tc/menu_m.sh
+exit 0
