baa56c237bbac577ffdd4827f016a88a27a355fe
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-04-17 23:19:29 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -3479,10 +3479,15 @@ function getredpillko() {
 #        sudo mv /home/tc/custom-module/rp-$ORIGIN_PLATFORM-4.4.180-prod.ko /home/tc/custom-module/redpill.ko
 #  else
 
-    if [ $MODEL == "FS2500" ]||[ $MODEL == "DS1019+" ]||[ $MODEL == "SA3600" ]; then
+    if [ $MODEL == "FS2500" ]||[ $MODEL == "SA3600" ]; then
     
         echo "Downloading peter's ${ORIGIN_PLATFORM} 4.4.180 ${MODEL} redpill.ko ..."
         sudo curl --insecure --location --progress-bar "https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/ext/rp-lkm/redpill-linux-v4.4.180+.ko" --output /home/tc/custom-module/redpill.ko
+
+    elif [ $MODEL == "DS1019+" ]; then
+
+        echo "Downloading peter's ${ORIGIN_PLATFORM} 4.4.180 ${MODEL} redpill-linux-ds1019+-v4.4.180+.ko -> redpill.ko ..."
+        sudo curl --insecure --location --progress-bar "https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/ext/rp-lkm/redpill-linux-ds1019+-v4.4.180+.ko" --output /home/tc/custom-module/redpill.ko
 
     elif [ $MODEL == "DS723+" ]; then
     
