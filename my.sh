2afc93755988c01ab461fee68f914c7f03034a7e
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-04-26 19:49:22 +0900

    Update my.sh

--- my.sh
+++ my.sh
@@ -380,11 +380,9 @@ DN_MODEL="$(echo $MODEL | sed 's/+/%2B/g')"
 echo "DN_MODEL is $DN_MODEL"
 
 cecho p "DSM PAT file pre-downloading in progress..."
-if [ $TARGET_REVISION == "64551" ]; then
-    URL="https://global.synologydownload.com/download/DSM/release/7.2/${TARGET_REVISION}/DSM_${DN_MODEL}_${TARGET_REVISION}.pat"
-else
-    URL="https://global.download.synology.com/download/DSM/release/${TARGET_VERSION}/${TARGET_REVISION}${SUVP}/DSM_${DN_MODEL}_${TARGET_REVISION}.pat"
-fi
+
+URL="https://global.download.synology.com/download/DSM/release/${TARGET_VERSION}/${TARGET_REVISION}${SUVP}/DSM_${DN_MODEL}_${TARGET_REVISION}.pat"
+
 cecho y "$URL"
 
 patfile="/mnt/${tcrppart}/auxfiles/${SYNOMODEL}.pat"                                         
