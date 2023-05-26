2b7a16612c25f35ad1cc94bd316e6bcf5a3dd081
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-05-26 22:02:27 +0900

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -3,12 +3,12 @@
 while [ -z "$GATEWAY_INTERFACE" ]; do
     clear
     if [ -f /home/tc/buildstatus ]; then
-      echo -e "\e[35m-------------------M-Shell for TCRP Loader Build Staus---------------------\e[0m"
-      echo -e "\e[33mStage\e[0m		\e[37mStatus\e[0m		Message"
-      echo -e "\e[35m---------------------------------------------------------------------------\e[0m"      
+      echo -e "\e[33m-------------------M-Shell for TCRP Loader Build Staus---------------------\e[0m"
+      echo -e "\e[33mStage\e[0m		\e[32mStatus\e[0m		Message"
+      echo -e "\e[33m---------------------------------------------------------------------------\e[0m"      
       cat /home/tc/buildstatus
     else
-      echo -e "\e[35m----------------------user_config.json extra_cmdline-----------------------\e[0m"
+      echo -e "\e[33m----------------------user_config.json extra_cmdline-----------------------\e[0m"
       jq '.extra_cmdline' /home/tc/user_config.json
     fi
     echo "Press ctrl-c to exit"
