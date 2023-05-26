2ce028245e47538c5b7fba8066e9dcbbcb2c59d8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-05-26 19:18:53 +0900

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
-# Set DateTime
-timezone="UTC"
-
-while true; do
-  if [ $(ifconfig | grep -i "inet " | grep -v "127.0.0.1" | wc -l) -gt 0 ]; then
-    break
-  fi
-  sleep 1
-  echo "Waiting for internet activation!!!"
-done
-
-#Get Timezone
-tz=$(curl -s ipinfo.io | grep timezone | awk '{print $2}' | sed 's/,//')
-if [ $(echo $tz | grep Seoul | wc -l ) -gt 0 ]; then
-    ntpserver="asia.pool.ntp.org"
-else
-    ntpserver="pool.ntp.org"
-fi
-
-if [ "$(which ntpclient)_" == "_" ]; then
-    tce-load -iw ntpclient 2>&1 >/dev/null
-fi    
-export TZ="${timezone}"
-echo "Synchronizing dateTime with ntp server $ntpserver ......"
-sudo ntpclient -s -h ${ntpserver} 2>&1 >/dev/null
-echo
-echo "DateTime synchronization complete!!!"
-
 while [ -z "$GATEWAY_INTERFACE" ]; do
     clear
-    echo "----------------------user_config.json extra_cmdline-----------------------"
-    jq '.extra_cmdline' /home/tc/user_config.json
+    if [ -f /home/tc/buildstatus ]; then
+      echo "----------------------------Loader Build Staus-----------------------------"
+      cat /home/tc/buildstatus
+    else
+      echo "----------------------user_config.json extra_cmdline-----------------------"
+      jq '.extra_cmdline' /home/tc/user_config.json
+    fi
     echo "Press ctrl-c to exit"
-    sleep 2
+    sleep 1
 done
