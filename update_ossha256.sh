ff0a074d3f7143dc04280ccf212add6d5fb260e1
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-10 23:18:43 +0900

    Update update_ossha256.sh

--- update_ossha256.sh
+++ update_ossha256.sh
@@ -4,4 +4,23 @@ echo "modify custom_config.json on $model"
 value=`grep $model ./kernel-bs-fb-patch-for-all/files-chksum | grep os.sha256|awk '{print $1}'`
 id=$(echo "$model" | sed 's/DS/ds/' | sed 's/RS/rs/' | sed 's/+/p/' | sed 's/DVA/dva/' | sed 's/FS/fs/' | sed 's/SA/sa/' "-7.2.0-64570")
 echo "id = $id"
+
+jsonfile=$(jq --arg id "$id" --arg value "$value" '
+  .build_configs |= map(
+    if (.id == $id) then
+      .downloads.os |= (
+        if has("sha256") then
+          .sha256 = $value
+        else
+          . + { "sha256": $value }
+        end
+      )
+    else
+      .
+    end
+  )
+' ./custom_config.json)
+
+echo $jsonfile | jq . > ./custom_config.json
+
 done 
