8d9fc9199d413a85c97ff38052fa265315eb9fab
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-19 11:35:59 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -791,7 +791,7 @@ while true; do
   fi
   
   dialog --backtitle "`backtitle`" --default-item "${MODEL}" --no-items \
-    --menu "Choose a model\n" 0 0 0 \
+    --menu "Choose a model\n" 0 0 7 \
     --file "${TMP_PATH}/mdl" 2>${TMP_PATH}/resp
   [ $? -ne 0 ] && return
   resp=$(<${TMP_PATH}/resp)
