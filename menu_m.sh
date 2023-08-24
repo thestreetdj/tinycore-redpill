89241b8409f21fe6b152e7047ea2d94b711d93c3
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-08-24 18:22:05 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -910,6 +910,7 @@ function setSuggest() {
 function storagepanel() {
   storage_options="TOWER_1_Bay TOWER_2_Bay TOWER_4_Bay TOWER_4_Bay_J TOWER_4_Bay_S TOWER_5_Bay TOWER_6_Bay TOWER_8_Bay TOWER_12_Bay RACK_2_Bay RACK_4_Bay RACK_8_Bay RACK_10_Bay RACK_12_Bay RACK_12_Bay_2 RACK_16_Bay RACK_20_Bay RACK_24_Bay RACK_60_Bay"
 
+  BAYSIZE="${bay}"
   while true; do
     echo "" > "${TMP_PATH}/size"
     echo "${storage_options}" | tr ' ' '\n' >> "${TMP_PATH}/size"
