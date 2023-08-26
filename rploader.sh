8681110939216ea5b135f4d59aaa079875379260
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-08-26 15:53:55 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -3552,7 +3552,7 @@ function getredpillko() {
     sudo rm -f /home/tc/custom-module/*.ko
     unzip /tmp/rp-lkms.zip        rp-${ORIGIN_PLATFORM}-${KVER}-prod.ko.gz -d /tmp >/dev/null 2>&1
     gunzip -f /tmp/rp-${ORIGIN_PLATFORM}-${KVER}-prod.ko.gz >/dev/null 2>&1
-    mv -f /tmp/rp-${ORIGIN_PLATFORM}-${KVER}-prod.ko /home/tc/custom-module/redpill.ko
+    sudo mv -f /tmp/rp-${ORIGIN_PLATFORM}-${KVER}-prod.ko /home/tc/custom-module/redpill.ko
 
 }
 
