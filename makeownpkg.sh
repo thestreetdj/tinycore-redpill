19688be7f82c91237e8d3b98d457a30f0964e1b5
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-11 12:56:34 +0900

    Update makeownpkg.sh

--- makeownpkg.sh
+++ makeownpkg.sh
@@ -10,12 +10,12 @@ if [ ! -d pkg/usr/local/tce.installed ]; then
   mkdir -p pkg/usr/local/tce.installed
 fi 
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
   echo "There is no new package name : ex) ./makeownpkg.sh newpkgname dependpkg1 dependpkg2 dependpkg3 dependpkg4 dependpkg5"
   exit 0
 fi
 
-if [ $# -eq 2 ]; then
+if [ $# -lt 2 ]; then
   echo "There is no dependency package name : ex) ./makeownpkg.sh newpkgname dependpkg1 dependpkg2 dependpkg3 dependpkg4 dependpkg5"
   exit 0
 fi
@@ -25,18 +25,18 @@ sudo cp -f ${1}.tcz /mnt/${tcrppart}/cde/optional
 grep -q "${1}.tcz" /mnt/${tcrppart}/cde/onboot.lst || echo "${1}.tcz" >> /mnt/${tcrppart}/cde/onboot.lst
 
 touch /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
-if [ $# -gt 3 ]; then
+if [ $# -gt 2 ]; then
   echo "${2}.tcz" >> /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
 fi
-if [ $# -gt 4 ]; then
+if [ $# -gt 3 ]; then
   echo "${3}.tcz" >> /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
 fi
-if [ $# -gt 5 ]; then
+if [ $# -gt 4 ]; then
   echo "${4}.tcz" >> /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
 fi
-if [ $# -gt 6 ]; then
+if [ $# -gt 5 ]; then
   echo "${5}.tcz" >> /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
 fi
-if [ $# -gt 7 ]; then
+if [ $# -gt 6 ]; then
   echo "${6}.tcz" >> /mnt/${tcrppart}/cde/optional/${1}.tcz.dep
 fi
