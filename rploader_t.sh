74f2525a279ac419809b62055a8dd6839fe9ffbe
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-21 00:15:10 +0900

    Update rploader_t.sh

--- rploader_t.sh
+++ rploader_t.sh
@@ -1831,8 +1831,8 @@ function backup() {
 
     thread=$(lscpu |grep CPU\(s\): | awk '{print $2}')
     if [ $(cat /usr/bin/filetool.sh | grep pigz | wc -l ) -eq 0 ]; then
-        sudo sed -i 's/\-czvf/\-cvf \- \| pigz -p ${thread} \>/g' /usr/bin/filetool.sh
-        sudo sed -i 's/\-czf/\-cf \- \| pigz -p ${thread} \>/g' /usr/bin/filetool.sh
+        sudo sed -i "s/\-czvf/\-cvf \- \| pigz -p "${thread}" \>/g" /usr/bin/filetool.sh
+        sudo sed -i "s/\-czf/\-cf \- \| pigz -p "${thread}" \>/g" /usr/bin/filetool.sh
     fi
     
 #    loaderdisk=$(mount | grep -i optional | grep cde | awk -F / '{print $3}' | uniq | cut -c 1-3)
