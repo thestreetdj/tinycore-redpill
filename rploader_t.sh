11f894ed11b82bb6071c3c36475651dcee442e70
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-22 08:26:57 +0900

    Update rploader_t.sh

--- rploader_t.sh
+++ rploader_t.sh
@@ -2605,7 +2605,7 @@ function savedefault {
     saved_entry="\${chosen}"
     save_env --file \$prefix/grubenv saved_entry
     echo -e "----------={ M Shell for TinyCore RedPill JOT }=----------"
-    echo "TCRP JOT Version : 0.9.5.0"
+    echo "TCRP JOT Version : 0.9.7.0"
     echo -e "Running on $(cat /proc/cpuinfo | grep "model name" | awk -F: '{print $2}' | wc -l) Processor $(cat /proc/cpuinfo | grep "model name" | awk -F: '{print $2}' | uniq)"
     echo -e "$(cat /tmp/tempentry.txt | grep earlyprintk | head -1 | sed 's/linux \/zImage/cmdline :/' )"
 }    
