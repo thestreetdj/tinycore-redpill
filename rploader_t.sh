2936d77c4cb62905e60d2febd00047d758cf1d33
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-21 23:57:34 +0900

    Update rploader_t.sh

--- rploader_t.sh
+++ rploader_t.sh
@@ -2719,13 +2719,14 @@ st "downloadtools" "Extraction tools" "Tools downloaded"
 st "extensions" "Extensions collection" "Extensions collection..."
     addrequiredexts
 st "loader.img" "Creation loader file" "Compile n make loader.img file."
+    UPPER_ORIGIN_PLATFORM=$(echo ${ORIGIN_PLATFORM} | tr '[:lower:]' '[:upper:]')
     if [ "$JUNLOADER" == "YES" ]; then
         echo "jun build option has been specified, so JUN MOD loader will be created"
         # jun's mod must patch using custom.gz from the first partition, so you need to fix the partition.
         sed -i "s/BRP_OUT_P2}\/\${BRP_CUSTOM_RD_NAME/BRP_OUT_P1}\/\${BRP_CUSTOM_RD_NAME/g" /home/tc/redpill-load/build-loader.sh        
-        sudo BRP_JUN_MOD=1 BRP_DEBUG=0 BRP_USER_CFG=user_config.json ./build-loader.sh $MODEL $TARGET_VERSION-$TARGET_REVISION loader.img
+        sudo BRP_JUN_MOD=1 BRP_DEBUG=0 BRP_USER_CFG=user_config.json ./build-loader.sh $MODEL $TARGET_VERSION-$TARGET_REVISION loader.img ${UPPER_ORIGIN_PLATFORM}
     else
-        sudo ./build-loader.sh $MODEL $TARGET_VERSION-$TARGET_REVISION loader.img
+        sudo ./build-loader.sh $MODEL $TARGET_VERSION-$TARGET_REVISION loader.img ${UPPER_ORIGIN_PLATFORM}
     fi
 st "copyfiles" "Copying files to P1,P2" "Copied boot files to the loader"
 #    msgnormal "======Unmount the ramdisk for add extensions.======="
