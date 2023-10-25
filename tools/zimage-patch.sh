1d8b8d8d5d3e84650f7cfd1e3a7b979a3b52e1a4
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-25 18:32:49 +0900

    Update zimage-patch.sh

--- tools/zimage-patch.sh
+++ tools/zimage-patch.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-. functions.sh
+# Define paths
+TMP_PATH="/tmp"
+LOG_FILE="${TMP_PATH}/log.txt"
+
+loaderdisk="$(blkid | grep "6234-C863" | cut -c 1-8 | awk -F\/ '{print $3}')"
+
+export BOOTLOADER_PATH="/mnt/${loaderdisk}1"
+export SLPART_PATH="/mnt/${loaderdisk}2"  # Synologic partition
+export CACHE_PATH="/mnt/${loaderdisk}3"
+
+ORI_ZIMAGE_FILE="${SLPART_PATH}/zImage"
+MOD_ZIMAGE_FILE="${CACHE_PATH}/zImage-dsm"
 
 set -o pipefail # Get exit code from process piped
 
@@ -12,15 +23,15 @@ echo -n "Patching zImage"
 rm -f "${MOD_ZIMAGE_FILE}"
 echo -n "."
 # Extract vmlinux
-/opt/arpl/bzImage-to-vmlinux.sh "${ORI_ZIMAGE_FILE}" "${TMP_PATH}/vmlinux" >"${LOG_FILE}" 2>&1 || dieLog
+./tool/bzImage-to-vmlinux.sh "${ORI_ZIMAGE_FILE}" "${TMP_PATH}/vmlinux" >"${LOG_FILE}" 2>&1 || dieLog
 echo -n "."
 # Patch boot params and ramdisk check
-/opt/arpl/kpatch "${TMP_PATH}/vmlinux" "${TMP_PATH}/vmlinux-mod" >"${LOG_FILE}" 2>&1 || dieLog
+./tool/kpatch "${TMP_PATH}/vmlinux" "${TMP_PATH}/vmlinux-mod" >"${LOG_FILE}" 2>&1 || dieLog
 echo -n "."
 # rebuild zImage
-/opt/arpl/vmlinux-to-bzImage.sh "${TMP_PATH}/vmlinux-mod" "${MOD_ZIMAGE_FILE}" >"${LOG_FILE}" 2>&1 || dieLog
+./tool/vmlinux-to-bzImage.sh "${TMP_PATH}/vmlinux-mod" "${MOD_ZIMAGE_FILE}" >"${LOG_FILE}" 2>&1 || dieLog
 echo -n "."
 # Update HASH of new DSM zImage
 HASH="`sha256sum ${ORI_ZIMAGE_FILE} | awk '{print$1}'`"
-writeConfigKey "zimage-hash" "${HASH}" "${USER_CONFIG_FILE}"
+#writeConfigKey "zimage-hash" "${HASH}" "${USER_CONFIG_FILE}"
 echo
