a0578fd6fceb4d8c47efe663ff2e0c8d936064de
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-11-05 16:45:55 +0900

    Update sngen.sh

--- sngen.sh
+++ sngen.sh
@@ -1,143 +1,9 @@
 #!/bin/bash
 
-function beginArray() {
+##### INCLUDES #########################################################################################################
+source myfunc.h
 
-    case $1 in
-    DS3615xs)
-        permanent="LWN"
-        serialstart="1130 1230 1330 1430"
-        ;;
-    DS3617xs)
-        permanent="ODN"
-        serialstart="1130 1230 1330 1430"
-        ;;
-    DS916+)
-        permanent="NZN"
-        serialstart="1130 1230 1330 1430"
-        ;;
-    DS918+)
-        permanent="PDN"
-        serialstart="1780 1790 1860 1980"
-        ;;
-    DS620slim)
-        permanent="PDN"
-        serialstart="1780 1790 1860 1980"
-        ;;
-    DS1019+)
-        permanent="PDN"
-        serialstart="1780 1790 1860 1980"
-        ;;
-    DS720+)
-        permanent="SBR"
-        serialstart="2030 2040 20C0 2150"
-        ;;
-    DS920+)
-        permanent="SBR"
-        serialstart="2030 2040 20C0 2150"
-        ;;
-    DS1520+)
-        permanent="TRR"
-        serialstart="2270"
-        ;;    
-    DS3622xs+)
-        permanent="SQR"
-        serialstart="2030 2040 20C0 2150"
-        ;;
-    DS1621xs+)
-        permanent="S7R"
-        serialstart="2080"
-        ;;
-    DS923+)
-        permanent="TQR"
-        serialstart="2270"
-        ;;
-    DS1522+)
-        permanent="TRR"
-        serialstart="2270"
-        ;;
-    DS723+)
-        permanent="TQR"
-        serialstart="2270"
-        ;;
-    DS1621+)
-        permanent="S7R"
-        serialstart="2080"
-        ;;
-    DS1819+)
-        permanent="RFR"
-        serialstart="1930 1940"
-        ;;
-    DS1821+)
-        permanent="S7R"
-        serialstart="2080"
-        ;;
-    DS1823xs+)
-        permanent="V5R"
-        serialstart="22B0"
-        ;;
-    DS2422+)
-        permanent="S7R"
-        serialstart="2080"
-        ;;
-    FS2500)
-        permanent="PSN"
-        serialstart="1960"
-        ;;
-    FS6400)
-        permanent="PSN"
-        serialstart="1960"
-        ;;
-    DVA3219)
-        permanent="RFR"
-        serialstart="1930 1940"
-        ;;
-    DVA3221)
-        permanent="SJR"
-        serialstart="2030 2040 20C0 2150"
-        ;;
-    DVA1622)
-        permanent="UBR"
-        serialstart="2030 2040 20C0 2150"
-        ;;
-    RS1221+)
-        permanent="RWR"
-        serialstart="20B0"
-        ;;
-    RS1619xs+)
-        permanent="QPR"
-        serialstart="1920"
-        ;;
-    RS3618xs)
-        permanent="ODN"
-        serialstart="1130 1230 1330 1430"
-        ;;
-    RS3413xs+)
-        permanent="S7R"
-        serialstart="2080"
-        ;;
-    RS3621xs+)
-        permanent="SZR"
-        serialstart="20A0"
-        ;;
-    RS4021xs+)
-        permanent="T2R"
-        serialstart="2250"
-        ;;
-    SA3400)
-        permanent="RJR"
-        serialstart="1920"
-        ;;
-    SA6400)
-        permanent="TQR"
-        serialstart="2270"
-        ;;
-    *)
-        permanent=""
-        serialstart=""
-        ;;        
-    esac
-
-}
+getvarsmshell "$1"
 
 function random() {
         printf "%06d" $(($RANDOM % 30000 + 1))
@@ -174,10 +40,7 @@ function generateMacAddress() {
 
 function generateSerial() {
 
-    beginArray $1
-
     case $1 in
-
     DS3615xs)
         serialnum="$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(random)
         ;;
@@ -275,7 +138,7 @@ function generateSerial() {
         serialnum=$(toupper "$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(generateRandomLetter)$(generateRandomValue)$(generateRandomValue)$(generateRandomValue)$(generateRandomValue)$(generateRandomLetter))
         ;;
     *)    
-        serialnum=""
+        serialnum="$(echo "$serialstart" | tr ' ' '\n' | sort -R | tail -1)$permanent"$(random)
         ;;  
     esac
 
@@ -301,15 +164,8 @@ EOF
 
 }
 
-MODELS="DS3615xs DS3617xs DS916+ DS918+ DS1019+ DS620slim DS920+ DS3622xs+ FS6400 DVA3219 DVA3221 DVA1622 DS1621+ DS1819+ DS1821+ DS1823xs+ DS1621xs+ RS4021xs+ DS2422+ DS1520+ FS2500 RS3618xs RS3413xs+ DS923+ DS723+ DS1522+ SA6400 DS720+ RS1221+ RS1619xs+ RS3621xs+ SA3400"
-
 if [ -z "$1" ]; then
     showhelp
 else
-    if [ $(echo ${MODELS} | grep $1 | wc -l ) -eq 0 ]; then
-        echo "Error : $1 is not an available model for serial number generation. "
-        echo "Available Models : ${MODELS}"
-    else
-        echo $(generateSerial $1)
-    fi
+    echo $(generateSerial $1)
 fi
