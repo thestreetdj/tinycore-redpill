f32dbc1b81149369e677b58027db8bc8d9a6ec4c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-12-03 19:58:06 +0900

    Create prepare-offline.bat

---
+++ prepare-offline.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/PeterSuh-Q3/tcrp-modules.git
+git clone https://github.com/PeterSuh-Q3/tcrp-addons.git
+git clone https://github.com/PeterSuh-Q3/redpill-load.git
+rm -r ./tcrp-modules/.git/ -Force
+mkdir auxfiles
