27a133da9dd6ba96441357c58349d130a88b0730
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-18 00:32:06 +0900

    Update rploader.sh

--- rploader.sh
+++ rploader.sh
@@ -3060,6 +3060,7 @@ st "frienddownload" "Friend downloading" "TCRP friend copied to /mnt/${loaderdis
         sudo curl -kL https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/main/rr/linuxrc.syno.impl -o /home/tc/rd.temp/linuxrc.syno.impl
         sudo curl -kL https://github.com/PeterSuh-Q3/tinycore-redpill/raw/main/rr/addons.tgz -o /tmp/addons.tgz
         sudo tar xvfz /tmp/addons.tgz -C /home/tc/rd.temp
+        sudo tar xvfz /home/tc/rd.temp/exts/all-modules/sbin.tgz -C /home/tc/rd.temp
     fi
     sudo chmod +x /home/tc/rd.temp/usr/sbin/modprobe    
     
