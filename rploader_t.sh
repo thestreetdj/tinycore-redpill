589a95d92967d242ad68cd0e9548b941da282cca
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-06 12:40:19 +0900

    Update rploader_t.sh

--- rploader_t.sh
+++ rploader_t.sh
@@ -3088,7 +3088,7 @@ fi
     # 1.0.2.2 Recycle initrd-dsm instead of custom.gz (extract /exts)
     if [ -f /mnt/${loaderdisk}3/initrd-dsm ]; then
         echo "Found initrd-dsm and extract /exts from " 
-        sudo cpio -idm < /mnt/${loaderdisk}3/initrd-dsm | grep '/exts'
+        cat /mnt/${loaderdisk}3/initrd-dsm | sudo cpio -idm "*exts*"
         sudo cp -vf /home/tc/custom-module/redpill.ko /home/tc/rd.temp/usr/lib/modules/redpill.ko
         sudo cp -vf ${HOMEPATH}/tools/modprobe /home/tc/rd.temp/usr/sbin/modprobe
     else
