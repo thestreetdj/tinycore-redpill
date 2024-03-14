156c190007b6608797b76ed57ee9e5a0e0361fb5
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-14 11:09:32 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -99,8 +99,8 @@ prevent_init="OFF"
 # Trap Ctrl+C and call ctrl_c function
 trap ctrl_c INT
 
-for edisk in $(fdisk -l | grep "Disk /dev/sd" | awk '{print $2}' | sed 's/://' ); do
-    if [ $(fdisk -l | grep "83 Linux" | grep ${edisk} | wc -l ) -eq 3 ]; then
+for edisk in $(sudo fdisk -l | grep "Disk /dev/sd" | awk '{print $2}' | sed 's/://' ); do
+    if [ $(sudo fdisk -l | grep "83 Linux" | grep ${edisk} | wc -l ) -eq 3 ]; then
         loaderdisk="$(blkid | grep ${edisk} | grep "6234-C863" | cut -c 1-8 | awk -F\/ '{print $3}')"    
     fi    
 done
