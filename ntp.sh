08f2692428c95186205a58ce154d5d5186d79d28
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-05-17 00:47:56 +0900

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -13,7 +13,7 @@ while [ -z "$GATEWAY_INTERFACE" ]; do
       jq '.extra_cmdline' /home/tc/user_config.json
       echo -e "\e[33m----------------------user_config.json synoinfo----------------------------\e[0m"
       jq '.synoinfo' /home/tc/user_config.json
-      echo -e "\e[33m----------------------added addon------------------------------------------\e[0m"
+      echo -e "\e[33m----------------------added addons-----------------------------------------\e[0m"
       ls /home/tc/redpill-load/custom/extensions
     fi
     echo "Press ctrl-c to exit"
