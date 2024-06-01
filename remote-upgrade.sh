d6badabc6c08552e08810a78101302841bb49f1e
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-06-01 10:48:39 +0900

    Create remote-upgrade.sh

---
+++ remote-upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Proceed with remote upgrade of the loader of TCRP-mshell."
+[ ! -d /mnt/p1 ] &&  mkdir /mnt/p1 || exit 99
+[ ! -d /mnt/p2 ] &&  mkdir /mnt/p2 || exit 99
+[ ! -d /mnt/p3 ] &&  mkdir /mnt/p3 || exit 99
+
+cd /dev/
+mount -t vfat synoboot1 /mnt/p1
+mount -t vfat synoboot2 /mnt/p2
+mount -t vfat synoboot3 /mnt/p3
+
+
+
+cd /mnt
+umount /mnt/p1
+umount /mnt/p2
+umount /mnt/p3
