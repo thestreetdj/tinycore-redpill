6ecbf022e7218be42a6fd0cabcc39d489f0abeea
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-10-25 23:16:26 +0900

    Update prepare-offline.bat

--- prepare-offline.bat
+++ prepare-offline.bat
@@ -1,4 +1,4 @@
-git clone -c core.autocrlf=false https://github.com/PeterSuh-Q3/tcrp-modules.git
-git clone -c core.autocrlf=false https://github.com/PeterSuh-Q3/tcrp-addons.git
-git clone -c core.autocrlf=false https://github.com/PeterSuh-Q3/redpill-load.git
+git clone --depth=1 -c core.autocrlf=false https://github.com/PeterSuh-Q3/tcrp-modules.git
+git clone --depth=1 -c core.autocrlf=false https://github.com/PeterSuh-Q3/tcrp-addons.git
+git clone --depth=1 -c core.autocrlf=false https://github.com/PeterSuh-Q3/redpill-load.git
 mkdir auxfiles
