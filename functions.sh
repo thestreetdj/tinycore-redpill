e4cac3af52f448456c71d56c383375716d10a043
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-10-29 01:18:51 +0900

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1658,7 +1658,7 @@ st "iscached" "Caching pat file" "Patfile ${SYNOMODEL}.pat is cached"
                   echo "Copy encrypted pat file : ${patfile} to ~/data/in"
                   mv -f ${patfile} ~/data/in/${SYNOMODEL}.pat
                   echo "Extracting encrypted pat file : ~/data/in/${SYNOMODEL}.pat to ~/data/out"
-                  sudo docker run --rm -v ~/data:/data syno-extract-system-patch ~/data/in/${SYNOMODEL}.pat /data/out/. || echo "extract latest pat"
+                  sudo docker run --rm -v ~/data:/data syno-extract-system-patch /data/in/${SYNOMODEL}.pat /data/out/. || echo "extract latest pat"
                   mv -f ~/data/out/* ${temp_pat_folder}
                 else
                   echo "Copy encrypted pat file : ${patfile} to ${temp_dsmpat_folder}"
